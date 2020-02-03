cb72a20c01e4f05b21e71ca4007f6ac9f91c41c9
Cameron <cameronriice@gmail.com>
2020-02-03 17:09:17 -0500

    EXP-002 Added audio handling, cleaned up code.

--- Julia.py
+++ Julia.py
@@ -1,6 +1,8 @@
 import math
 import sys
 import os
+from os import path
+from datetime import datetime
 
 import pandas as pd
 
@@ -8,20 +10,47 @@ import pygame
 import pygame.gfxdraw
 
 #constants
-black = [0, 0, 0]
-white = [255,255,255]
+BLACK = [0, 0, 0]
+GREY = [10, 10, 10]
+WHITE = [255, 255, 255]
 
-# Screen params
+DATA_OUTPUT = "./outputs"
+AUDIO_INPUT = "./audio"
+
+# Screen
 screen = pygame.display.set_mode(flags=pygame.FULLSCREEN)
-width, height = pygame.display.get_surface().get_size()
 
+width, height = pygame.display.get_surface().get_size()
 center = (int(width/2), int(height/2))
 max_dist = math.hypot(*center)
 
-rec = False
-frame = 0
+# Audio
+pygame.mixer.init()
+audio = pygame.mixer.music
+audio_file = ''
 
-# Mouse cursor properties
+if path.exists(AUDIO_INPUT) and path.isdir(AUDIO_INPUT):
+    for _, _, files in os.walk(AUDIO_INPUT):
+        if files:
+            audio_file = files[0]
+            audio.load(path.join(AUDIO_INPUT, audio_file))
+            audio.set_endevent(pygame.constants.USEREVENT)
+
+        break
+
+else:
+    os.mkdir(AUDIO_INPUT)
+    print(f"*** ERROR ***: Must place audio file within {AUDIO_INPUT} folder!")
+
+# Data output
+output_name = ''
+
+if not (path.exists(DATA_OUTPUT) and path.isdir(DATA_OUTPUT)):
+    os.mkdir(DATA_OUTPUT)
+
+output_name = f"{DATA_OUTPUT}/{audio_file[0].split('.')[0] if audio_file else '!NA!'}-{str(datetime.now()).split(' ')[0]}.csv"
+
+# Mouse cursor
 cur = pygame.mouse
 cur.set_visible(False)
 
@@ -32,44 +61,52 @@ cursor_data = {
     'vel_x': [],
     'vel_y': [],
     'frame': [],
-    #'timestamp': []
+    'audio_ms': []
 }
 
-# for use with the get_color function, normalize the dist from 0-255
-def normalize_dist(x):
-    return (255)*((x)/(max_dist))
-
-# Can change cursor color based on proximity to center
+#get cursor color based on proximity to center, return Tuple(R,G,B)
 def get_color(dist):
-    n = normalize_dist(dist)
-    return (int(n), int((255 - n)), int(0))
+    # Normalize distance value to 0 - 255 range
+    normalized = (255) * ((dist) / (max_dist))
+    return (int(normalized), int((255 - normalized)), int(0))
 
 # Draw middle "reference" cross
-def draw_ref_cross():
-    pygame.gfxdraw.hline(screen, int(width/3), width - int(width/3), int(height/2), white)
-    pygame.gfxdraw.vline(screen, int(width/2), height - int(height/4), int(height/4), white)
+def draw_ref_cross(size):
+    pygame.gfxdraw.hline(screen, center[0] - size, center[0] + size, center[1], WHITE)
+    pygame.gfxdraw.vline(screen, center[0], center[1] - size, center[1] + size, WHITE)
+
+def draw_grid(size):
+    # Get number of segements
+    x_segments = width / size
+    y_segments = height / size
+
+    # Draw vertical segments, draws right then left starting from center.
+    for seg in range(int(x_segments / 1.5)):
+        pygame.gfxdraw.vline(screen, center[0] + int(seg * size), 0, height, GREY)
+        pygame.gfxdraw.vline(screen, center[0] - int(seg * size), 0, height, GREY)
+
+    # As above, now drawing up then down from center
+    for seg in range(int(y_segments / 1.5)):
+        pygame.gfxdraw.hline(screen, 0, width, center[1] + int(seg * size), GREY)
+        pygame.gfxdraw.hline(screen, 0, width, center[1] - int(seg * size), GREY)
+
 
 # Draw mouse circle
-def draw_mouse_circle(x, y, r, color=(255,255,255), filled=False, dist=0):
+def draw_mouse_circle(x, y, r, color=(255, 255, 255), filled=False, dist=0):
     if filled:
         pygame.gfxdraw.filled_circle(screen, x, y, r, get_color(dist))
+
     else:
-        pygame.gfxdraw.circle(screen, x, y, r, color)
+        pygame.gfxdraw.circle(screen, x, y, r, get_color(dist))
 
 # generate the output csv with cursor frame data
 def generate_output():
     df = pd.DataFrame(cursor_data)
-    df.to_csv('./outputs/test.csv', index=False)
+    df.to_csv(output_name, index=False)
     return
 
-# ! work in progress - replay functionality !
-#Load previous mouse movement data to replay
-# replay_data = pd.read_csv('test.csv')
-
-# def replay(frame):
-#     if frame < len(replay_data.index):
-#         data = replay_data.iloc[frame]
-#         cur.set_pos(data['cursor_x'], data['cursor_y'])
+rec = False
+frame = 0
 
 # Main loop
 while True:
@@ -78,35 +115,46 @@ while True:
             generate_output()  
             sys.exit()
 
-    # Watch for mouse click to begin recording, right click to stop
-    if cur.get_pressed()[0] and not rec:
-        rec = True
-        frame = 0
-    elif cur.get_pressed()[2] and rec:
-        rec = False
+        if event.type == pygame.constants.USEREVENT:
+            pygame.event.post(pygame.event.Event(pygame.QUIT))
+
+        elif event.type == pygame.KEYDOWN:
+            if event.key == 27:
+                pygame.event.post(pygame.event.Event(pygame.QUIT))
+
+            elif event.key == 32:
+                rec = True
+                frame = 0
+                if audio_file:
+                    audio.play()
+
+            else:
+                print(event.key)
 
     # Get cursor position and relative velocity
     cur_x, cur_y = cur.get_pos()
     vel_x, vel_y = cur.get_rel()
-    
+
     # Calculate distance from Center (ref_cross)
     dist = round(math.hypot(abs(cur_x-center[0]), abs(cur_y-center[1])), 2)
 
     # Record data
     if rec:
-#         replay(frame)
         cursor_data['cursor_x'].append(cur_x) 
         cursor_data['cursor_y'].append(cur_y)
         cursor_data['dist'].append(dist)
         cursor_data['vel_x'].append(abs(vel_x)) 
         cursor_data['vel_y'].append(abs(vel_y))
         cursor_data['frame'].append(frame)
+        cursor_data['audio_ms'].append(audio.get_pos())
 
         frame += 1
 
-    screen.fill(black)
+    # Draw to scren
+    screen.fill(BLACK)
+    draw_grid(50)
+    draw_ref_cross(10)
+    draw_mouse_circle(cur_x, cur_y, 10, filled=True, dist=dist) 
 
-    draw_ref_cross()
-    draw_mouse_circle(cur_x, cur_y, 20, filled=True, dist=dist) 
-
+    # Refresh screen
     pygame.display.flip()
